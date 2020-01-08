df1e21b1b8d4ba185053a50bcd9422e16368cc33
lpfsno1 <51092131+lpfsno1@users.noreply.github.com>
2020-01-08 13:11:17 +0800

    Update weather.js

--- weather.js
+++ weather.js
@@ -133,7 +133,7 @@ let config = {
     darksky_api: "c126f503822fb598bf788f8763859c0c", //从https://darksky.net/dev/ 上申请key填入即可
     aqicn_api: "fe1a5360ff2a8328989597e67596c5118532d10e", //从http://aqicn.org/data-platform/token/#/ 上申请key填入即可
     huweather_apiKey: "6d77d1192c1a4d319e4c747e1091a077", //和风天气APIkey,可自行前往 https://dev.heweather.com/ 进行获取
-    lat_lon: "41.798992,123.447995", //请填写经纬度,直接从谷歌地图中获取即可
+    lat_lon: "41.7989272300,123.4481348600", //请填写经纬度,直接从谷歌地图中获取即可
     lang: "zh", //语言,请不要修改
     show: {
         log: 0, //调试日志,0为不开启,1为开启,2为开启精简日志
